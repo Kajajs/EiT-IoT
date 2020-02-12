c359859e3b5cc52df2d8db1222d8a8bee04cd684
soms2 <sosevle@stud.ntnu.no>
2020-02-12 15:26:21 +0100

    Added UTC to timestamp

--- communication/fetch.py
+++ communication/fetch.py
@@ -1,7 +1,5 @@
 from nbiot import nbiot
-import json
-import time
-#from asyncio import *
+import json, time, datetime
 
 address = "https://api.nbiot.telenor.io"
 api_token = "bde332ec188ee5e679deda22b7770b66f9b2a2ccd6f7f15a8154fbeeb8825534"
@@ -9,6 +7,6 @@ collectionId = "17dh0cf43jg03b"
 deviceId = "17dh0cf43jg755"
 
 client = nbiot.Client(addr = address, token = api_token)
-data = client.collection_data(collectionId, since = 0.0, until = 1)
+data = client.collection_data(collectionId, limit = 1)
 for i in data:
-    print(f"{i.payload} {i.received}")
+    print(f"{i.payload.decode('utf-8')} {i.received.timestamp()}")
