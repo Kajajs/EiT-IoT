87f3cc5e0dc2af5404fce83250f0c8464c42a5b9
sosevle <soms2008@hotmail.com>
2020-03-03 21:15:29 +0100

    Added basics for reading aplification values

--- microphone/microphone.ino
+++ microphone/microphone.ino
@@ -1,36 +1,24 @@
 #define LITTLE 0
 #define MUCH 1
 
-int counter = -1;
-int measurements [100];
-int val;
+long val = 0;
+int readval;
 
 void setup(){ 
   Serial.begin(9600); // open serial port, set the baud rate to 9600 bps
 }
 
-int average(){
-  int total = 0;
-  for (int i = 0; i < 100; i++){
-    total = total + measurements[i];
-  }
-  return (int) total / 100;
-}
-
 void loop(){
   
-  counter++;
-  if (counter > 99){
-    counter = 0;
-    Serial.println(average());
-  }
-  measurements[counter] = analogRead(A0);   //connect mic sensor to Analog 0
-  delay(10);
-  /*
-  val = analogRead(A0);
-  if (val > 545) {
-    Serial.print("Sound=");
-    Serial.println(val,DEC);//print the sound value to serial  
+  for (int i = 0; i < 5000; i++){
+    readval = (int) analogRead(A0);
+    if (readval > val){
+      val = readval;
     }
-  */
+    //val = val + (long) analogRead(A0);
+    //delay(10);
+  }
+  Serial.println(val);
+  val = 0;
+  
 }
